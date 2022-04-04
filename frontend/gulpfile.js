1524fc25c40426152cb3b75f0d2ee6beca42fefe
LucaSalvigni <salvigni.luca97@gmail.com>
2022-04-04 16:26:09 +0200

    Refactoring

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,72 +1,80 @@
-const { src, dest, series } = require('gulp');
-const del = require('del');
-const fs   = require('fs');
-const zip = require('gulp-zip');
-const log = require('fancy-log');
-const exec = require('child_process').exec;
+const { src, dest, series } = require("gulp");
+const del = require("del");
+const fs = require("fs");
+const zip = require("gulp-zip");
+const log = require("fancy-log");
+const exec = require("child_process").exec;
 
 const paths = {
-  prod_build: '../frontend-build',
-  vue_src: '../frontend/dist/**/*',
-  vue_dist: '../frontend-build/frontend/dist',
-  zipped_file_name: 'frontend-nodejs.zip'
+  prod_build: "../frontend-build",
+  vue_src: "../frontend/dist/**/*",
+  vue_dist: "../frontend-build/frontend/dist",
+  zipped_file_name: "frontend-nodejs.zip",
 };
 
-function clean()  {
-  log('removing the old files in the directory')
-  return del('../frontend-build/**', {force:true});
+function clean() {
+  log("removing the old files in the directory");
+  return del("../frontend-build/**", { force: true });
+}
+
+function qualityAssurance(cb) {
+  log("assurance Vue's code quality");
+  return exec("npm run lint", function (err, stdout, stderr) {
+    log(stdout);
+    log(stderr);
+    cb(err);
+  });
 }
 
 function buildVueCodeTask(cb) {
-  log('building Vue code into the directory')
-  return exec('cd ../frontend && npm run build', function (err, stdout, stderr) {
+  log("building Vue code into the directory");
+  return exec("npm run build", function (err, stdout, stderr) {
     log(stdout);
     log(stderr);
     cb(err);
-  })
+  });
 }
 
 function doTest(cb) {
-  log('testing Vue code')
-  return exec('npm run test', function(err, stdout, stderr) {
+  log("testing Vue code");
+  return exec("npm run test", function (err, stdout, stderr) {
     log(stdout);
     log(stderr);
     cb(err);
-  })
+  });
 }
 
 function createProdBuildFolder() {
-
   const dir = paths.prod_build;
-  log(`Creating the folder if not exist  ${dir}`)
-  if(!fs.existsSync(dir)) {
+  log(`Creating the folder if not exist  ${dir}`);
+  if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
-    log('📁  folder created:', dir);
+    log("📁  folder created:", dir);
   }
 
-  return Promise.resolve('the value is ignored');
+  return Promise.resolve("the value is ignored");
 }
 
 function copyVueCodeTask() {
-  log('copying Vue code into the directory')
-  return src(`${paths.vue_src}`)
-        .pipe(dest(`${paths.vue_dist}`));
+  log("copying Vue code into the directory");
+  return src(`${paths.vue_src}`).pipe(dest(`${paths.vue_dist}`));
 }
 
 function zippingTask() {
-  log('zipping the code ')
+  log("zipping the code ");
   return src(`${paths.prod_build}/**`)
-      .pipe(zip(`${paths.zipped_file_name}`))
-      .pipe(dest(`${paths.prod_build}`))
+    .pipe(zip(`${paths.zipped_file_name}`))
+    .pipe(dest(`${paths.prod_build}`));
 }
 
 function deleteDistBuildFolder() {
-  log('deleting dist build folder')
-  return del('./dist', {force:true})
+  log("deleting dist build folder");
+  return del("./dist", { force: true });
 }
 
 exports.default = series(
   clean,
+  qualityAssurance,
   buildVueCodeTask,
   doTest,
   createProdBuildFolder,
