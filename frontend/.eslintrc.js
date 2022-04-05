9ab700bc9e196e969d9e9cfcb1ceeb6d9fa5ff8f
LucaSalvigni <salvigni.luca97@gmail.com>
2022-04-05 12:09:37 +0200

    Add new rules for code's quality

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
     node: true,
   },
   extends: [
+    "plugin:vue/base",
     "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-strongly-recommended",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "plugin:prettier/recommended",
   ],
@@ -14,5 +17,22 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/no-empty-component-block": "error",
+    /**"vue/html-comment-content-newline": ["error",
+      {
+        "singleline": "always" | "never" | "ignore",
+        "multiline": "always" | "never" | "ignore",
+      },
+      {
+        "exceptions": []
+      }
+    ],
+    "vue/html-comment-content-spacing": ["error",
+      "always" | "never",
+      {
+        "exceptions": []
+      }
+    ],
+    "vue/html-comment-indent": ["error", type],**/
   },
 };
