5e74ee5097554301254a384817f58e039399ce06
LucaSalvigni <salvigni.luca97@gmail.com>
2022-04-05 12:10:05 +0200

    Add new file for test HelloWorld component

---
+++ frontend/test/HelloWorld.test.js
@@ -0,0 +1,20 @@
+/**
+ * @vitest-enviroment happy-dom
+ */
+
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import HelloWorld from '../src/components/HelloWorld.vue'
+
+describe('HelloWorld Component Test', ()=> {
+    it('should mount', () => {
+        const wrapper = mount(HelloWorld, {
+            props: {
+                msg: "Hello from Luca"
+            },
+        })
+        expect(wrapper.text()).toContain('Hello from Luca')
+
+        expect(wrapper.find('h3').exists()).toBeTruthy()
+    })
+})
