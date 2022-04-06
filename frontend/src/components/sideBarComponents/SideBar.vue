55621c0b959e8b7050128245eda8bef085d55736
LucaSalvigni <salvigni.luca97@gmail.com>
2022-04-06 15:20:22 +0200

    Add a Sidebar

---
+++ frontend/src/components/sideBarComponents/SideBar.vue
@@ -0,0 +1,168 @@
+<!-- Custom Sidebar component -->
+<template>
+  <div>
+    <div class="sidebar flex flex-col">
+      <router-link to="/">
+        <img
+          src="@/assets/logo.png"
+          class="mask w-40 h-40 my-5 ml-5 mask-squircle"
+        />
+      </router-link>
+
+      <div class="dropdown mt-5">
+        <SidebarLink
+          class="barMenu pl-1.5"
+          to=""
+          icon="fas fa-bars"
+        ></SidebarLink>
+        <ul
+          tabindex="0"
+          class="block p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
+        >
+          <li class="inline-block">
+            <SidebarLink
+              class="w-44 profile-drop mb-3 pl-0.5"
+              to="/profile"
+              icon="fas fa-user-cog"
+              >Profile</SidebarLink
+            >
+          </li>
+          <li class="inline-block">
+            <SidebarLink
+              class="leaderboard-drop w-44 mb-3 pl-0.5"
+              to="/leaderboard"
+              icon="fas fa-chart-bar"
+              >Leaderboard</SidebarLink
+            >
+          </li>
+        </ul>
+      </div>
+      <SidebarLink class="w-44 home mb-3 pl-0.5" to="/" icon="fas fa-home"
+        >Home</SidebarLink
+      >
+      <SidebarLink
+        class="w-44 profile mb-3 pl-0.5"
+        to="/profile"
+        icon="fas fa-user-cog"
+        >Profile</SidebarLink
+      >
+      <SidebarLink
+        class="leaderboard w-44 mb-3 pl-0.5"
+        to="/leaderboard"
+        icon="fas fa-chart-bar"
+        >Leaderboard</SidebarLink
+      >
+
+      <!--<div v-if="!$store.loading.value">-->
+      <SidebarLink
+        class="w-44 login mt-96 p-0.5"
+        to="/login"
+        icon="fas fa-user-lock"
+        >Sign in</SidebarLink
+      >
+      <!--</div>-->
+    </div>
+  </div>
+</template>
+
+<script>
+import SidebarLink from "./SidebarLink";
+
+export default {
+  name: "SideBar",
+  components: {
+    SidebarLink,
+  },
+};
+</script>
+
+<style>
+:root {
+  --sidebar-bg-color: #1f1e1e;
+  --sidebar-item-hover: #151414;
+  --sidebar-item-active: #151414;
+}
+</style>
+
+<style scoped>
+.sidebar {
+  color: white;
+  background-color: var(--sidebar-bg-color);
+  transition: 0.3s ease;
+}
+.dropdown-content {
+  background-color: #666666;
+}
+.barMenu {
+  visibility: hidden;
+  width: 0;
+  height: 0;
+}
+
+@media only screen and (max-height: 1200px) {
+  .login {
+    margin-top: 10rem;
+  }
+  .indicator {
+    margin-top: 5rem;
+  }
+}
+
+@media only screen and (max-width: 1200px) {
+  img {
+    max-width: 4rem;
+    max-height: 4rem;
+    height: 4rem;
+    width: 4rem;
+    margin: 0.5rem;
+  }
+  .home,
+  .profile,
+  .leaderboard,
+  .login {
+    width: 3rem;
+    padding-left: 0.8rem;
+    padding-right: 0.1rem;
+  }
+  .notifications {
+    width: 3rem;
+    padding-left: 1rem;
+  }
+  .home,
+  .profile,
+  .leaderboard {
+    margin: 0;
+    margin-top: 0.5rem;
+  }
+}
+
+@media only screen and (max-width: 785px) {
+  .sidebar {
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+  .barMenu {
+    visibility: visible;
+    width: 3rem;
+    height: 3rem;
+    padding-left: 0.9rem;
+  }
+  .login {
+    width: auto;
+    margin-top: 0rem;
+  }
+  .home,
+  .profile,
+  .leaderboard {
+    display: none;
+  }
+  .notifications,
+  .indicator {
+    width: auto;
+    margin-top: 0.7rem;
+  }
+  .login button {
+    display: none;
+  }
+}
+</style>
---
+++ frontend/src/components/sideBarComponents/SidebarLink.vue
@@ -0,0 +1,92 @@
+<!-- Custom component that represent a link for the Sidebar -->
+<template>
+  <router-link
+    :to="to"
+    class="this.to self-center link rounded-lg flex"
+    :class="{ active: isActive }"
+  >
+    <i class="icon" :class="icon" />
+    <button class="btn btn-ghost"><slot /></button>
+  </router-link>
+</template>
+
+<script>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+export default {
+  props: {
+    to: { type: String, required: true },
+    icon: { type: String, required: true },
+    click: { type: String, required: false, default: "" },
+  },
+  setup(props) {
+    const route = useRoute();
+    const isActive = computed(() => route.path === props.to);
+    return { isActive };
+  },
+};
+</script>
+
+<style scoped>
+button {
+  font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif,
+    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  font-style: italic;
+  font-weight: 700;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.1s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.link {
+  align-items: center;
+  color: #cdcbcb;
+  text-decoration: none;
+  transition: 0.2s;
+}
+button:hover {
+  background-color: transparent;
+}
+.link:hover {
+  background-color: var(--sidebar-item-hover);
+}
+.link.active {
+  background-color: var(--sidebar-item-active);
+}
+.link .icon {
+  width: 25px;
+  margin-right: 10px;
+}
+
+@media only screen and (max-width: 1200px) {
+  .btn {
+    visibility: hidden;
+    width: 0;
+  }
+  .icon {
+    width: 1.3rem;
+    height: 1.3rem;
+  }
+}
+
+@media only screen and (max-width: 785px) {
+  .btn {
+    visibility: visible;
+    width: auto;
+  }
+  .login button,
+  .indicator button {
+    display: none;
+  }
+  .icon {
+    width: 2rem;
+    height: 2rem;
+  }
+}
+</style>
