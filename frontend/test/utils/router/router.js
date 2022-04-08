0680c9be660c16c26bb6fc8e17c916edbc644965
LucaSalvigni <salvigni.luca97@gmail.com>
2022-04-08 15:06:53 +0200

    Refactoring

--- frontend/test/utils/router/router.js
+++ frontend/test/utils/router/router.js
@@ -1,33 +1,47 @@
-import { createRouter, createMemoryHistory } from "vue-router";
-import HomeView from "../../../src/views/Home.vue";
+import { createRouter, createWebHistory } from "vue-router";
 import Error404 from "../../../src/views/ErrorView.vue";
+import HomeView from "../../../src/views/Home.vue";
+import LeaderBoard from "../../../src/views/LeaderBoard.vue";
+import LogIn from "../../../src/views/LogIn.vue";
+import Profile from "../../../src/views/Profile.vue";
+import SignUp from "../../../src/views/SignUp.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "home",
-        component: HomeView,
-    },
-    {
-        path: "/about",
-        name: "about",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: function () {
-        return import(/* webpackChunkName: "about" */ "../src/views/AboutView.vue");
-        },
-    },
-    {
-        path: "/404",
-        name: "error404",
-        component: Error404,
-    },
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/404",
+    name: "error404",
+    component: Error404,
+  },
+  {
+    path: "/login",
+    name: "LogIn",
+    component: LogIn,
+  },
+  {
+    path: "/signup",
+    name: "SignUp",
+    component: SignUp,
+  },
+  {
+    path: "/leaderboard",
+    name: "LeaderBoard",
+    component: LeaderBoard,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+  },
 ];
 
-const router = createRouter({ 
-    history: createMemoryHistory(),
-    routes 
-})
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
